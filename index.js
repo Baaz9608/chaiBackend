902a73463394a6ebb79b869f22a5681624555af1
BaazShah606 <shahbazmd3002@gmail.com>
2023-11-06 01:45:38 +0530

    one feature added

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get('/youtube', (req, res)=>{
     res.send('<h2>Chai aur code</h2>')
 })
 
+app.get('/github', (req, res)=>{
+    res.json({username:"baazshah"})
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Example app listening on port ${port}`);
 })
